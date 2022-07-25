6183d15203b62d773c53cafb986668253f1b5082
Takeshi Kondo <ncukondo@gmail.com>
2022-07-25 09:47:54 +0000

    refactor tasks

--- main.ts
+++ main.ts
@@ -1,28 +1,28 @@
-import { readZip } from "https://deno.land/x/jszip@0.11.0/mod.ts";
-import { Buffer } from "https://deno.land/std@0.149.0/io/buffer.ts";
-import { join, parse } from "https://deno.land/std@0.148.0/path/mod.ts";
+import { readZip } from "jszip";
+import { Buffer } from "buffer";
+import { join, parse } from "path";
 
 type RawEntry = {
   type: string;
   text: string;
   ts?: string;
   username?: string;
-  user_profile?:{
+  user_profile?: {
     name?: string;
-    real_name?:string;
-    display_name?:string;
-  }
-}
+    real_name?: string;
+    display_name?: string;
+  };
+};
 
 const convertJsonToMessagObj = (name: string, data: RawEntry) => {
-  const unixtime =Number.parseFloat(data?.ts ?? "") * 1000 
-  const timestamp =  unixtime ? new Date(unixtime).toISOString() : "";
+  const unixtime = Number.parseFloat(data?.ts ?? "") * 1000;
+  const timestamp = unixtime ? new Date(unixtime).toISOString() : "";
   const channel = name.split("/")[0] ?? "";
   const text = data.text ?? "";
   const real_name = data.user_profile?.real_name ?? "";
   const display_name = data.user_profile?.display_name ?? "";
   const user = data.username ?? data.user_profile?.name ?? "";
-  const result = { timestamp, channel, text, user,real_name,display_name };
+  const result = { timestamp, channel, text, user, real_name, display_name };
   return result;
 };
 
@@ -41,7 +41,7 @@ const extractMessagesFromJson = (files: { name: string; data: RawEntry }[]) => {
     if (!Array.isArray(data)) return [];
     return data.flatMap((content) => {
       if (content?.type !== "message") return [];
-      return convertJsonToMessagObj(name,content);
+      return convertJsonToMessagObj(name, content);
     });
   });
 };
