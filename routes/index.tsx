bcd4c677a7c6ab9df395192481eb0c75fb9a3e2b
Takeshi Kondo <ncukondo@gmail.com>
2022-07-27 12:07:10 +0000

    change message

--- routes/index.tsx
+++ routes/index.tsx
@@ -6,14 +6,9 @@ import FileInput from "../islands/FileInput.tsx";
 export default function Home() {
   return (
     <div class={tw`p-4 mx-auto max-w-screen-md`}>
-      <img
-        src="/logo.svg"
-        height="100px"
-        alt="the fresh logo: a sliced lemon dripping with juice"
-      />
+      
       <p class={tw`my-6`}>
-        Welcome to `fresh`. Try update this message in the ./routes/index.tsx
-        file, and refresh.
+        Input zip file exported from slack.
       </p>
       <FileInput />
     </div>
