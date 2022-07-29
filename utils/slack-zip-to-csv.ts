9ff92b58f3ab2bd96cebec2b4d38302257e567d4
Takeshi Kondo <ncukondo@gmail.com>
2022-07-29 06:00:11 +0000

    refactor slack-zip-to-csv

--- utils/slack-zip-to-csv.ts
+++ utils/slack-zip-to-csv.ts
@@ -1,10 +1,12 @@
 import { JSZip } from "jszip";
 
-type MessageEntry = {
-  type: string;
+type MessageEntry = UserMessageEntry | OtherMessageEntry;
+
+type UserMessageEntry = {
+  type: "message";
   text: string;
-  ts?: string;
-  username?: string;
+  ts: string;
+  user: string;
   user_profile?: {
     name?: string;
     real_name?: string;
@@ -12,45 +14,122 @@ type MessageEntry = {
   };
 };
 
-const convertJsonToMessagObj = (name: string, data: MessageEntry) => {
-  const unixtime = Number.parseFloat(data?.ts ?? "") * 1000;
-  const timestamp = unixtime ? new Date(unixtime).toISOString() : "";
-  const channel = name.split("/")[0] ?? "";
-  const text = data.text ?? "";
-  const real_name = data.user_profile?.real_name ?? "";
-  const display_name = data.user_profile?.display_name ?? "";
-  const user = data.username ?? data.user_profile?.name ?? "";
-  const result = { timestamp, channel, text, user, real_name, display_name };
+type OtherMessageEntry = {
+  type: "message";
+  sub_type: "channel_join" | "bot_message" | "bot_add";
+  text: string;
+  ts: string;
+  user: string;
+  username?: string;
+};
+
+type UserEntry = {
+  id: string;
+  team_id: string;
+  name: string;
+  deleted: boolean;
+  real_name: string;
+  tz: string;
+  profile: {
+    real_name: string;
+    display_name: string;
+    first_name: string;
+    last_name: string;
+  };
+};
+
+const convertOtherMessage = (
+  { message: { username } }: {
+    message: OtherMessageEntry;
+  },
+) => {
+  const user_name = username ?? "";
+  return { user_name };
+};
+
+const convertUserMessage = (
+  { message, user }: {
+    message: UserMessageEntry;
+    user?: UserEntry;
+  },
+) => {
+  const user_id = message.user ?? "";
+  const real_name = message.user_profile?.real_name ?? "";
+  const display_name = message.user_profile?.display_name ?? "";
+  const user_name = user?.name ?? "";
+  const result = {
+    user_name,
+    real_name,
+    display_name,
+    user_id,
+  };
   return result;
+};
+
+const convertMessage = (
+  { message, channel, users }: {
+    message: MessageEntry;
+    channel: string;
+    users?: Record<string, UserEntry>;
+  },
+) => {
+  const user_id = message.user ?? "";
+  const unixtime = Number.parseFloat(message?.ts ?? "") * 1000;
+  const timestamp = unixtime ? new Date(unixtime).toISOString() : "";
+  const text = message.text ?? "";
+  const result = { channel, timestamp, text, user_id };
+  if ("sub_type" in message) {
+    return { ...result, ...convertOtherMessage({ message }) };
+  } else {
+    const user = users?.[user_id];
+    return { ...result, ...convertUserMessage({ message, user }) };
+  }
 };
 
 const readJsonsInZip = async (zipFile: ArrayBuffer) => {
   const zip = await new JSZip().loadAsync(zipFile);
-  const names = [...zip]
-    .map((z) => z.name)
-    .filter((name) => name.endsWith(".json"))
-    .sort();
-  const jsonTexts = await Promise.all(names.map(async (name) => {
+  const names = [...zip].map((z) => z.name).filter((name) =>
+    name.endsWith(".json")
+  );
+  const texts = await Promise.all(names.map(async (name) => {
     return { name, text: (await zip.file(name).async("string")) };
   }));
-  return jsonTexts.map(({ name, text }) => ({ name, data: JSON.parse(text) }));
+  return texts.map(({ name, text }) => ({ name, data: JSON.parse(text) }));
 };
 
-const extractMessagesFromJson = (
-  files: { name: string; data: MessageEntry }[],
+const getUserInfo = (
+  files: { name: string; data: unknown }[],
 ) => {
-  return files.flatMap(({ name, data }) => {
-    if (!Array.isArray(data)) return [];
-    return data.flatMap((content) => {
-      if (content?.type !== "message") return [];
-      return convertJsonToMessagObj(name, content);
-    });
-  });
+  const userEntries = (files
+    .find(({ name }) => name === "users.json")
+    ?.data as UserEntry[])
+    ?.map((entry) => [entry.id, entry] as [string, UserEntry]);
+  return userEntries && Object.fromEntries(userEntries);
+};
+
+const extractMessagesFromFiles = (
+  files: { name: string; data: unknown }[],
+) => {
+  const users = getUserInfo(files);
+
+  const messages = files
+    .filter(({ data }) => Array.isArray(data))
+    .map(({ name, data: entries }) => {
+      const channel = name.split("/")[0] ?? "";
+      return (entries as UserMessageEntry[])
+        .filter((data) => data?.type === "message")
+        .map((message) => ({ channel, message, users }));
+    })
+    .flat();
+  return messages.map(convertMessage);
 };
 
 const extractMessagesFromZip = async (zipFile: ArrayBuffer) => {
   const files = await readJsonsInZip(zipFile);
-  return extractMessagesFromJson(files);
+  const sorted = files.sort(({ name: a }, { name: b }) =>
+    (a > b && 1) || (a < b && -1) || 0
+  );
+  return extractMessagesFromFiles(sorted);
 };
 
 const objectsToTable = (objList: Record<string, string>[]) => {
@@ -80,7 +159,6 @@ const convertSlackLogToCsv = async (zipFile: ArrayBuffer) => {
   const messages = await extractMessagesFromZip(zipFile);
   const table = objectsToTable(messages);
   return toCSV(table);
-  //return await (addBOM(csv));
 };
 
 export { convertSlackLogToCsv, readJsonsInZip };
